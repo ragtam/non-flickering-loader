99a528734f53c87f4a63477458e65fbd94fe6a61
mat <mag190033@gmail.com>
2022-12-30 00:15:25 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'non-flickering-loader';
+    title = 'non-flickering-loader';
 }
